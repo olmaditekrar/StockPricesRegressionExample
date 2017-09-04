edda616491b3c6e1c0b7efe47bc8724b1d6c5862
Onur Yücedağ <olmaditekrar@gmail.com>
2017-09-05 01:44:22 +0300

    Implemented a Label of forecast value for Adjusted Close Feature
    And also added comment to get a good knowledge . Adjusted Value feature shifted %1 up and assigned to the Forecast value ( 'Label')

--- StockPricesRegressionExample.py
+++ StockPricesRegressionExample.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import quandl
-
+import math
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
-print(df.head())
+forecast_col = 'Adj. Close'
+df.fillna(-99999,inplace=True) #If the data isn't exist ! We are filling this data with a high value .
+forecast_out = int(math.ceil(0.01*len(df))) # Forecast 1 percent of the data . This provide us to predict a price from 1 percent ealier prices of it .
+df['Label'] = df[forecast_col].shift(-forecast_out) # We shifted the label coloumn (Adj. Close oloumn before we shift) into the 1 percent future(Minus shifted) ! And assign it.
+#So now , we have a label that will present %1 of future price of the current row's Adj. Close feature ! 
+df.dropna(inplace=True) # Droppen NaN's out of the list !
+
+
+
+
+print(df.tail())
+print(forecast_out)
+print(df['Label'].tail())
