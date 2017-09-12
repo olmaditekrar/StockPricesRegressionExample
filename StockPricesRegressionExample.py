008cf945a7856e87685417a50cddf5a80976fef9
Onur Yücedağ <olmaditekrar@gmail.com>
2017-09-12 19:45:57 +0300

    Pickling the classifier

--- StockPricesRegressionExample.py
+++ StockPricesRegressionExample.py
@@ -6,6 +6,8 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
 import time
+import pickle
+
 
 style.use('ggplot')
 
@@ -31,13 +33,26 @@ X = X[:-forecast_out]
 df.dropna(inplace=True) # Droppen NaN's out of the list !
 
 y = np.array(df['Label']) # y is standby our labels !
-y = np.array(df['Label'])
-
+  
 #Now we are going to get test and train datas from X and y . 
 X_train , X_test , y_train , y_test = cross_validation.train_test_split(X,y,test_size = 0.2) # It will shuffle all the data inside X and y and it will return 4 outputs : X_train , y_train , X_test , y_test !
 
-clf = LinearRegression(n_jobs = 10) #Classifier is the Linear Regression and its so much better than Support Vector Machines . And we are threading 10 jobs at a time.
-clf.fit(X_train,y_train) # We fit the training data. 
+
+#TRAINING THE CLASSIFIER 
+
+	# clf = LinearRegression(n_jobs = 10) #Classifier is the Linear Regression and its so much better than Support Vector Machines . And we are threading 10 jobs at a time.
+	# clf.fit(X_train,y_train) # We fit the training data. 
+
+	# with open('linearregression.pickle','wb') as f: #Create a file and use f as a variable.
+	# 	pickle.dump(clf,f) #Dumb the trained classifier into that file.
+
+#END OF TRAINING THE CLASSIFIER
+
+#We don't need to train the classifier everytime we want to run the program , so we saved the trained classifier's data into the file and we simply read it .
+pickle_in = open ('linearregression.pickle','rb') # Open the file and assign it to the pickle_in.
+clf = pickle.load(pickle_in) # Load it into the classifier.
+ 
+
 accuracy = clf.score(X_test,y_test) # We test the data and get the accuracy score . 
 # The reason why we want different data to train and test is : We need to send different data for testing because it will be more learnable with the data it didn't see before.
 
